eedb70853bb029c6a4f5f76705b9f159aa62a0c8
Tim123456Tim <86416015+Tim123456Tim@users.noreply.github.com>
2023-06-08 16:29:10 +0400

    Initial Commit

---
+++ TrafficLights/TrafficLightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightsApp.swift
+//  TrafficLights
+//
+//  Created by Тимофей Юдин on 08.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
